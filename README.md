a598e498b83a885bba4cd8c2dfb85983a0b1e1fa
Jim Keener <jim@jimkeener.com>
2024-08-17 19:41:23 -0400

    first pass. littly messy, but works

--- README.md
+++ README.md
@@ -1,1 +1,179 @@
 # jskcalc
+
+A RPN calculator.
+
+Each cell can be a float (f64) or "word" (i64, but treated like bits).
+
+To quit, `q` or ^C.
+
+## Quick Example
+
+```
+45 d2r dup sin sq swap cos sq + p
+N 1.0
+2 sqrt 2 / 45 d2r cos - p
+N 0.0
+1 2 3 ps + ps - ps 5 p * p ps
+Stack is 5 entries deep
+ 0: Num(3.0)
+ 1: Num(2.0)
+ 2: Num(1.0)
+ 3: Num(0.0)
+ 4: Num(1.0)
+
+Stack is 4 entries deep
+ 0: Num(5.0)
+ 1: Num(1.0)
+ 2: Num(0.0)
+ 3: Num(1.0)
+
+Stack is 3 entries deep
+ 0: Num(-4.0)
+ 1: Num(0.0)
+ 2: Num(1.0)
+
+N 5.0
+N -20.0
+Stack is 3 entries deep
+ 0: Num(-20.0)
+ 1: Num(0.0)
+ 2: Num(1.0)
+
+clr
+654321 w p 9876543 w p ps & p pl ps
+W b0000 0000  0000 0000  0000 0000  0000 0000  0000 0000  0000 1001  1111 1011  1111 0001
+W b0000 0000  0000 0000  0000 0000  0000 0000  0000 0000  1001 0110  1011 0100  0011 1111
+Stack is 2 entries deep
+ 0: Word(9876543)
+ 1: Word(654321)
+
+W b0000 0000  0000 0000  0000 0000  0000 0000  0000 0000  0000 0000  1011 0000  0011 0001
+W b0000 0000  0000 0000  0000 0000  0000 0000  0000 0000  0000 0000  1011 0000  0011 0001
+W x00 00 00 00 00 00 B0 31
+W 45105i64
+W 45105u64
+Stack is 1 entries deep
+ 0: Word(45105)
+
+654321 w dup p 9876543 w dup p rot ps
+W b0000 0000  0000 0000  0000 0000  0000 0000  0000 0000  0000 1001  1111 1011  1111 0001  
+W b0000 0000  0000 0000  0000 0000  0000 0000  0000 0000  1001 0110  1011 0100  0011 1111  
+Stack is 5 entries deep
+ 0: Word(9876543)
+ 1: Word(654321)
+ 2: Word(9876543)
+ 3: Word(654321)
+ 4: Word(45105)
+
+& rot swap p drop p drop p     
+W b0000 0000  0000 0000  0000 0000  0000 0000  0000 0000  0000 1001  1111 1011  1111 0001  
+W b0000 0000  0000 0000  0000 0000  0000 0000  0000 0000  1001 0110  1011 0100  0011 1111  
+W b0000 0000  0000 0000  0000 0000  0000 0000  0000 0000  0000 0000  1011 0000  0011 0001
+
+q
+```
+
+## Stack
+drop
+: (1 -- )
+
+swap
+: (1 2 -- 2 1)
+
+rot
+: (1 2 3 -- 2 3 1)
+
+dup
+: duplicates the top of the stack
+
+clr
+: clear the stack
+
+## Printing
+ps
+: prints the stack
+
+p
+: prints the top of the stack
+
+pl
+: prints a more detailed version of the top of the stack
+
+## floats
+
+To push onto the stack, just enter the number. For details see the
+[rust f64 grammar](https://doc.rust-lang.org/std/primitive.f64.html#grammar).
+
+### binary operations
+- +
+- -
+- *
+- /
+- pow
+- atan2
+- hypot
+
+### unary operations
+- sin
+- cos
+- tan
+- asin
+- acos
+- atan
+- sinh
+- cosh
+- tanh
+- asinh
+- acosh
+- atanh
+- d2r
+- r2d
+- 1/
+- ln
+- ln1+
+- log10
+- log2
+- exp
+- exp-1
+- sqrt
+- cbrt
+- sq
+- cb
+- abs
+- ceil
+- floor
+
+### constants
+- pi
+- e
+- sqrt2
+- ln2
+- ln10
+- tau
+- pi/2
+- pi/4
+
+### misc
+- dms2dd
+- dd2dms
+
+## word
+
+To push a word, push an float then issue `w`. The integer portion of the float is considered as the word.
+
+### binary operations
+- w+
+- w-
+- w\*
+- w/
+- mod
+- &
+- ^
+- |
+- sl
+- lsr
+- asr
+
+### unary operations
+- ~
+
---
+++ src/main.rs
@@ -0,0 +1,272 @@
+#![feature(integer_sign_cast)]
+use std::f64::consts::{E, FRAC_PI_2, FRAC_PI_4, LN_10, LN_2, PI, SQRT_2, TAU};
+use std::io;
+use std::str::FromStr;
+
+#[derive(Debug, Clone)]
+enum Cell {
+    Num(f64),
+    Str(String),
+    Word(i64),
+}
+
+fn main() -> io::Result<()> {
+    let bops: Vec<(&str, &dyn Fn(f64, f64) -> f64)> = vec![
+        ("+", &|a, b| b + a),
+        ("-", &|a, b| b - a),
+        ("*", &|a, b| b * a),
+        ("/", &|a, b| b / a),
+        ("pow", &|a, b| a.powf(b)),
+        ("atan2", &f64::atan2),
+        ("hypot", &f64::hypot),
+    ];
+    let uops: Vec<(&str, &dyn Fn(f64) -> f64)> = vec![
+        ("sin", &f64::sin),
+        ("cos", &f64::cos),
+        ("tan", &f64::tan),
+        ("asin", &f64::asin),
+        ("acos", &f64::acos),
+        ("atan", &f64::atan),
+        ("sinh", &f64::sinh),
+        ("cosh", &f64::cosh),
+        ("tanh", &f64::tanh),
+        ("asinh", &f64::asinh),
+        ("acosh", &f64::acosh),
+        ("atanh", &f64::atanh),
+        ////
+        ("d2r", &f64::to_radians),
+        ("r2d", &f64::to_degrees),
+        ////
+        ("1/", &f64::recip),
+        ////
+        ("ln", &f64::ln),
+        ("ln1+", &f64::ln_1p),
+        ("log10", &f64::log10),
+        ("log2", &f64::log2),
+        ("exp", &f64::exp),
+        ("exp-1", &f64::exp_m1),
+        ("sqrt", &f64::sqrt),
+        ("cbrt", &f64::cbrt),
+        ("sq", &|a| a * a),
+        ("cb", &|a| a * a * a),
+        ////
+        ("abs", &f64::abs),
+        ("ceil", &f64::ceil),
+        ("floor", &f64::floor),
+    ];
+    let consts: Vec<(&str, f64)> = vec![
+        ("pi", PI),
+        ("e", E),
+        ("sqrt2", SQRT_2),
+        ("ln2", LN_2),
+        ("ln10", LN_10),
+        ("tau", TAU),
+        ("pi/2", FRAC_PI_2),
+        ("pi/4", FRAC_PI_4),
+    ];
+
+    // Not a fan of the cloning.
+    let stackop: Vec<(&str, usize, &dyn Fn(Vec<Cell>) -> Vec<Cell>)> = vec![
+        ("drop", 1, &|_v| vec![]),
+        ("swap", 2, &|v| vec![v[0].clone(), v[1].clone()]),
+        ("rot", 3, &|v| {
+            vec![v[0].clone(), v[2].clone(), v[1].clone()]
+        }),
+        ("dup", 1, &|v| vec![v[0].clone(), v[0].clone()]),
+        ////
+        ("w", 1, &|v| {
+            if let Cell::Num(n) = v[0] {
+                vec![Cell::Word(n as i64)]
+            } else {
+                let v = &v[0];
+                println!("not a number: {v:?}; cannot convert to word");
+                vec![]
+            }
+        }),
+        ////
+        ("dms2dd", 3, &|v| {
+            if let Cell::Num(d) = v[2] {
+                if let Cell::Num(m) = v[1] {
+                    if let Cell::Num(s) = v[0] {
+                        vec![Cell::Num(d + m / 60.0 + s / 3600.0)]
+                    } else {
+                        let s = &v[0];
+                        println!("s was not a number: {s:?}");
+                        vec![]
+                    }
+                } else {
+                    let m = &v[1];
+                    println!("m was not a number: {m:?}");
+                    vec![]
+                }
+            } else {
+                let d = &v[2];
+                println!("d was not a number: {d:?}");
+                vec![]
+            }
+        }),
+        ("dd2dms", 1, &|v| {
+            if let Cell::Num(mut v) = v[0] {
+                let d = v.floor();
+                v = (v - d) * 60.0;
+                let m = v.floor();
+                v = (v - m) * 60.0;
+                let s = v;
+                vec![Cell::Num(d), Cell::Num(m), Cell::Num(s)]
+            } else {
+                let d = &v[0];
+                println!("d was not a number: {d:?}");
+                vec![]
+            }
+        }),
+    ];
+
+    let wbops: Vec<(&str, &dyn Fn(i64, i64) -> i64)> = vec![
+        ("w+", &|a, b| b.wrapping_add(a)),
+        ("w-", &|a, b| b.wrapping_sub(a)),
+        ("w*", &|a, b| b * a),
+        ("w/", &|a, b| b / a),
+        ("mod", &|a, b| b % a),
+        ("&", &|a, b| b & a),
+        ("^", &|a, b| b ^ a),
+        ("|", &|a, b| b | a),
+        ("sl", &|a, b| b << a),
+        ("lsr", &|a, b| ((b as u64) >> a) as i64),
+        ("asr", &|a, b| b >> a),
+    ];
+    let wuops: Vec<(&str, &dyn Fn(i64) -> i64)> = vec![("~", &|a| !a)];
+    let mut stack = vec![];
+    let stdin = io::stdin();
+    loop {
+        let mut buffer = String::new();
+        stdin.read_line(&mut buffer)?;
+        let split = buffer.split(" ");
+        for s in split {
+            let s = s.trim();
+            if s.len() == 0 {
+                continue;
+            }
+            if let Ok(f) = f64::from_str(s) {
+                stack.push(Cell::Num(f));
+            } else {
+                if let Some(op) = bops.iter().position(|e| e.0 == s) {
+                    let op = bops[op].1;
+                    let a = stack.pop();
+                    let b = stack.pop();
+                    if let Some(Cell::Num(a)) = a {
+                        if let Some(Cell::Num(b)) = b {
+                            stack.push(Cell::Num(op(a, b)));
+                        } else {
+                            println!("2nd not a number: {b:?}");
+                        }
+                    } else {
+                        println!("1st not a number: {a:?}");
+                    }
+                } else if let Some(op) = uops.iter().position(|e| e.0 == s) {
+                    let op = uops[op].1;
+                    let a = stack.pop();
+                    if let Some(Cell::Num(a)) = a {
+                        stack.push(Cell::Num(op(a)));
+                    } else {
+                        println!("1st not a number: {a:?}");
+                    }
+                } else if let Some(op) = wbops.iter().position(|e| e.0 == s) {
+                    let op = wbops[op].1;
+                    let a = stack.pop();
+                    let b = stack.pop();
+                    if let Some(Cell::Word(a)) = a {
+                        if let Some(Cell::Word(b)) = b {
+                            stack.push(Cell::Word(op(a, b)));
+                        } else {
+                            println!("2nd not a number: {b:?}");
+                        }
+                    } else {
+                        println!("1st not a number: {a:?}");
+                    }
+                } else if let Some(op) = wuops.iter().position(|e| e.0 == s) {
+                    let op = wuops[op].1;
+                    let a = stack.pop();
+                    if let Some(Cell::Word(a)) = a {
+                        stack.push(Cell::Word(op(a)));
+                    } else {
+                        println!("1st not a number: {a:?}");
+                    }
+                } else if let Some(op) = consts.iter().position(|e| e.0 == s) {
+                    stack.push(Cell::Num(consts[op].1));
+                } else if let Some(op) = stackop.iter().position(|e| e.0 == s) {
+                    let op = stackop[op];
+                    let mut vals = vec![];
+                    for i in 0..op.1 {
+                        if let Some(v) = stack.pop() {
+                            vals.push(v);
+                        } else {
+                            println!("No element at stack position {i}");
+                            break;
+                        }
+                    }
+                    if vals.len() == op.1 {
+                        stack.append(&mut op.2(vals))
+                    }
+                } else if s == "pl" {
+                    let v = stack.last();
+                    if let Some(v) = v {
+                        match v {
+                            Cell::Num(v) => println!("N {v:?}"),
+                            Cell::Word(v) => {
+                                print!("W b");
+                                for i in (0..=56).rev().step_by(8) {
+                                    let p1 = (v >> (i + 4)) & 0xf;
+                                    let p2 = (v >> i) & 0xf;
+                                    print!("{p1:04b} {p2:04b}  ");
+                                }
+                                println!();
+                                print!("W x");
+                                for i in (0..=56).rev().step_by(8) {
+                                    let p = (v >> i) & 0xff;
+                                    print!("{p:02X} ");
+                                }
+                                println!();
+                                println!("W {v}i64");
+                                let u = v.cast_unsigned();
+                                println!("W {u}u64");
+                                // let f = f64::from_bits(v.cast_unsigned());
+                                // println!("W {f}f64")
+                            }
+                            Cell::Str(v) => println!("S {v:?}"),
+                        };
+                    }
+                } else if s == "p" {
+                    let v = stack.last();
+                    if let Some(v) = v {
+                        match v {
+                            Cell::Num(v) => println!("N {v:?}"),
+                            Cell::Word(v) => {
+                                print!("W b");
+                                for i in (0..=56).rev().step_by(8) {
+                                    let p1 = (v >> (i + 4)) & 0xf;
+                                    let p2 = (v >> i) & 0xf;
+                                    print!("{p1:04b} {p2:04b}  ");
+                                }
+                                println!();
+                            }
+                            Cell::Str(v) => println!("S {v:?}"),
+                        };
+                    }
+                } else if s == "clr" {
+                    while stack.pop().is_some() {}
+                } else if s == "ps" {
+                    let l = stack.len();
+                    println!("Stack is {l} entries deep");
+                    for (i, e) in stack.iter().rev().enumerate() {
+                        println!(" {i}: {e:?}");
+                    }
+                    println!();
+                } else if s == "q" {
+                    return Ok(());
+                } else {
+                    stack.push(Cell::Str(s.to_owned()));
+                }
+            }
+        }
+    }
+}
